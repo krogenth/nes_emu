d1f8052c365d6ec1d4ed4ad26474bece814c1061
Daniel Kelley <daniel.kelley@millgroupinc.com>
2021-08-13 18:44:11 -0400

    add Nes_Snd_Emu library dependency to git submodules

--- build.bat
+++ build.bat
@@ -2,6 +2,14 @@ cd .\libs\sfml
 mkdir .\build
 cd .\build
 cmake -G "Visual Studio 16 2019" ..\
+MSBuild SFML.sln /property:configuration=Debug /property:Platform=x64
 MSBuild SFML.sln /property:configuration=Release /property:Platform=x64
+cd ..\..\Nes_Snd_Emu
+mkdir .\build
+cd .\build
+cmake -G "Visual Studio 16 2019" ..\
+MSBuild Nes_Snd_Emu.sln /property:configuration=Debug /property:Platform=x64
+MSBuild Nes_Snd_Emu.sln /property:configuration=Release /property:Platform=x64
 cd ..\..\..
+MSBuild .\vs\nes.sln /property:configuration=Debug /property:Platform=x64
 MSBuild .\vs\nes.sln /property:configuration=Release /property:Platform=x64
